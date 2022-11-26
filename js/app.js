880c2d017f55fe5e947e75ddc600d6cd84690172
Dominik Zemlik <dominikzemlik1@gmail.com>
2022-11-26 21:58:13 +0100

    Add validation for register value and remove blue background in autocomplete input

--- js/app.js
+++ js/app.js
@@ -1,4 +1,24 @@
 const form = document.querySelector('.form');
+const inputs = document.querySelectorAll('.form__input');
+const submitButton = document.querySelector('.form__button');
+
+const validateRegiser = (registerValue) => {
+	const reg = /[a-f][0-9]/;
+
+	if (!reg.test(registerValue) && registerValue !== '') {
+		alert(`Podana wartość ${registerValue} nie jest w kodzie szesnatkowym! Możliwość wykonania operacji została zablokowana!`);
+		submitButton.disabled = true;
+	} else {
+		submitButton.disabled = false;
+	}
+};
+
+inputs.forEach((input) => {
+	input.addEventListener('change', (e) => {
+		const inputValue = e.target.value;
+		validateRegiser(inputValue.toLowerCase());
+	});
+});
 
 const executeOperation = (e) => {
 	e.preventDefault();
@@ -11,13 +31,10 @@ const executeOperation = (e) => {
 		e.target[`${firstSelectedRegister.toLowerCase()}`].value = e.target[`${secondSelectedRegister.toLowerCase()}`].value;
 	} else {
 		// XCHG operation
-
 		const firstRegister = e.target[`${firstSelectedRegister.toLowerCase()}`].value;
-
 		const secondRegister = e.target[`${secondSelectedRegister.toLowerCase()}`].value;
 
 		e.target[`${firstSelectedRegister.toLowerCase()}`].value = secondRegister;
-
 		e.target[`${secondSelectedRegister.toLowerCase()}`].value = firstRegister;
 	}
 };
